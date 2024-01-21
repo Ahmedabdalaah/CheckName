d1340fe07780628c8a66061ebfe23a8c51ff6964
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-21 18:52:30 +0200

    Add project files.

---
+++ CheckName/Check.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CheckName
+{
+    internal class Check
+    {
+        public bool chekN (string input)
+        {
+            string expresion = "^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$";
+            if (Regex.IsMatch(input, expresion))
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CheckName/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CheckName;
+Check che = new Check();
+Console.WriteLine("Enter Input");
+try
+{
+    string input = Console.ReadLine();
+    if(input != "")
+    {
+        bool result = che.chekN(input);
+        if(result==true)
+        {
+            Console.WriteLine("Correct name");
+        }
+        else
+        {
+            Console.WriteLine("incorrect name");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input");
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
